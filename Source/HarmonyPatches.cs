c2b856680a4579ce932885374b6e9c2425e04b46
Travis Offtermatt <travisty2@gmail.com>
2020-03-02 00:14:57 -0700

    Updating for 1.1

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -1,8 +1,7 @@
-﻿using Harmony;
+﻿using HarmonyLib;
 using RimWorld;
 using System;
 using System.Reflection;
-using UnityEngine;
 using Verse;
 
 namespace SupplyAndDemand
@@ -14,7 +13,7 @@ namespace SupplyAndDemand
 
         static Main()
         {
-            var harmony = HarmonyInstance.Create("com.supplyanddemand.rimworld.mod");
+            var harmony = new Harmony("com.supplyanddemand.rimworld.mod");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
 
             Log.Message(
@@ -29,7 +28,7 @@ namespace SupplyAndDemand
     {
         static void Postfix(ref int __result, StockGenerator __instance)
         {
-            float adjustment = Settings.ScaleAdjustment * 0.001f;
+            float adjustment = Settings.ScaleAdjustment * 0.01f;
             float totalWealth = Find.CurrentMap.wealthWatcher.WealthTotal;
             float storyTellerPriceLossFactor = 1f - Find.Storyteller.difficulty.tradePriceFactorLoss;
 
@@ -46,73 +45,6 @@ namespace SupplyAndDemand
                     Log.Message("    __result post: " + __result);
                     Log.Message("");
                 }
-            }
-        }
-
-        public class SettingsController : Mod
-        {
-            public SettingsController(ModContentPack content) : base(content)
-            {
-                base.GetSettings<Settings>();
-            }
-
-            public override string SettingsCategory()
-            {
-                return "Supply And Demand";
-            }
-
-            public override void DoSettingsWindowContents(Rect inRect)
-            {
-                Settings.DoSettingsWindowContents(inRect);
-            }
-
-            public override void WriteSettings()
-            {
-                base.WriteSettings();
-            }
-        }
-
-        public class Settings : ModSettings
-        {
-            private const float MAX_SCALE = 400f;
-            public static bool ScaleVisitors;
-
-            public static float ScaleAdjustment = 1;
-
-            public static bool IncludeLogging = false;
-
-            public override void ExposeData()
-            {
-                base.ExposeData();
-
-                Scribe_Values.Look<bool>(ref ScaleVisitors, "ScaleVisitors", false, false);
-                Scribe_Values.Look<float>(ref ScaleAdjustment, "ScaleAdjustment", 100, false);
-
-                if (Scribe.mode == LoadSaveMode.LoadingVars)
-                {
-                    if (ScaleAdjustment < 1)
-                        ScaleAdjustment = 1;
-                    else if (ScaleAdjustment > MAX_SCALE)
-                        ScaleAdjustment = MAX_SCALE;
-                }
-            }
-
-            public static void DoSettingsWindowContents(Rect rect)
-            {
-                Listing_Standard l = new Listing_Standard(GameFont.Small)
-                {
-                    ColumnWidth = System.Math.Min(400, rect.width / 2)
-                };
-                
-                l.Begin(rect);
-                l.CheckboxLabeled("Scale Visitors?", ref ScaleVisitors);
-                l.Label("Scalar: " + ScaleAdjustment.ToString("n2") + "%");
-                ScaleAdjustment = l.Slider(ScaleAdjustment, 0.1f, MAX_SCALE);
-                l.End();
-                if (Widgets.ButtonText(new Rect(20, 110, 100, 30), "Reset"))
-                    ScaleAdjustment = 100;
-
-                Widgets.CheckboxLabeled(new Rect(0, 300, 150, 30), "Debug Logging", ref IncludeLogging);
             }
         }
     }
---
+++ Source/Settings.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace SupplyAndDemand
+{
+    public class SettingsController : Mod
+    {
+        public SettingsController(ModContentPack content) : base(content)
+        {
+            base.GetSettings<Settings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Supply And Demand";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+        }
+    }
+
+    public class Settings : ModSettings
+    {
+        private const float MIN_SCALE = 0.1f;
+        private const float MAX_SCALE = 1000f;
+        private const float SCALE_ADJUSTMENT_DEFAULT = 100f;
+
+        public static bool ScaleVisitors = false;
+
+        public static float ScaleAdjustment = SCALE_ADJUSTMENT_DEFAULT;
+        private static string textBuffer = SCALE_ADJUSTMENT_DEFAULT.ToString();
+
+        public static bool IncludeLogging = false;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look<bool>(ref ScaleVisitors, "ScaleVisitors", false, false);
+            Scribe_Values.Look<float>(ref ScaleAdjustment, "ScaleAdjustmentV2", 100, false);
+
+            if (Scribe.mode == LoadSaveMode.LoadingVars)
+            {
+                if (ScaleAdjustment < MIN_SCALE)
+                    ScaleAdjustment = MIN_SCALE;
+                else if (ScaleAdjustment > MAX_SCALE)
+                    ScaleAdjustment = MAX_SCALE;
+            }
+
+            textBuffer = ScaleAdjustment.ToString("n2");
+        }
+
+        public static void DoSettingsWindowContents(Rect rect)
+        {
+            string originalText = textBuffer;
+            float originalScaler = ScaleAdjustment;
+
+            float x = rect.x + 20;
+            float y = rect.y;
+            Widgets.CheckboxLabeled(new Rect(x, y, 150, 32), "Scale Visitors?", ref ScaleVisitors);
+            y += 60;
+
+            Widgets.Label(new Rect(x, y, 75, 32), "Scalar:");
+            textBuffer = GUI.TextField(new Rect(x + 80, y, 60, 32), textBuffer);
+            Widgets.Label(new Rect(x + 160, y, 40, 32), "%");
+            y += 40;
+
+            if (originalText != textBuffer)
+            {
+                float v;
+                if (float.TryParse(textBuffer, out v))
+                {
+                    if (v >= MIN_SCALE && v <= MAX_SCALE)
+                    {
+                        ScaleAdjustment = v;
+                    }
+                }
+            }
+            else
+            {
+                ScaleAdjustment = Widgets.HorizontalSlider(new Rect(x, y, 400, 32), ScaleAdjustment, MIN_SCALE, MAX_SCALE);
+                y += 40;
+                if (Widgets.ButtonText(new Rect(20, y, 100, 30), "Reset"))
+                    ScaleAdjustment = 100;
+
+                if (originalScaler != ScaleAdjustment)
+                    textBuffer = ScaleAdjustment.ToString("n2");
+            }
+
+            //Widgets.CheckboxLabeled(new Rect(0, 300, 150, 30), "Debug Logging", ref IncludeLogging);
+        }
+    }
+}
